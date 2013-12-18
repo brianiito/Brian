e999109b82da14c6fc187cf25aa4ad0f304caa73
brianiito <brianiito@yahoo.com>
2013-12-18 19:29:01 +0000

    00810112

---
+++ POSEvening/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace POSEvening
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.cboItems = new System.Windows.Forms.ComboBox();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.txtPaid = new System.Windows.Forms.TextBox();
+            this.txtDue = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colExtPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.splitContainer1.Panel2.Controls.Add(this.btnFinish);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.txtChange);
+            this.splitContainer1.Panel2.Controls.Add(this.txtPaid);
+            this.splitContainer1.Panel2.Controls.Add(this.txtDue);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(922, 385);
+            this.splitContainer1.SplitterDistance = 482;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.Yellow;
+            this.splitContainer2.Panel1.Controls.Add(this.label4);
+            this.splitContainer2.Panel1.Controls.Add(this.label3);
+            this.splitContainer2.Panel1.Controls.Add(this.txtQty);
+            this.splitContainer2.Panel1.Controls.Add(this.btnAddToCart);
+            this.splitContainer2.Panel1.Controls.Add(this.cboItems);
+            this.splitContainer2.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel1_Paint);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dgvCart);
+            this.splitContainer2.Size = new System.Drawing.Size(482, 385);
+            this.splitContainer2.SplitterDistance = 140;
+            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(100, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Product";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(175, 56);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 23);
+            this.txtQty.TabIndex = 2;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.BackColor = System.Drawing.Color.Blue;
+            this.btnAddToCart.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddToCart.ForeColor = System.Drawing.Color.White;
+            this.btnAddToCart.Location = new System.Drawing.Point(171, 95);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(118, 32);
+            this.btnAddToCart.TabIndex = 1;
+            this.btnAddToCart.Text = "Add To Cart";
+            this.btnAddToCart.UseVisualStyleBackColor = false;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // cboItems
+            // 
+            this.cboItems.FormattingEnabled = true;
+            this.cboItems.Items.AddRange(new object[] {
+            "Brown Bread",
+            "Brown Sugar",
+            "Horlicks",
+            "Nido",
+            "Peak Milk",
+            "Ovaltine"});
+            this.cboItems.Location = new System.Drawing.Point(175, 23);
+            this.cboItems.Name = "cboItems";
+            this.cboItems.Size = new System.Drawing.Size(299, 24);
+            this.cboItems.TabIndex = 0;
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.AllowUserToOrderColumns = true;
+            this.dgvCart.BackgroundColor = System.Drawing.Color.Blue;
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.colPrice,
+            this.Column2,
+            this.colExtPrice});
+            this.dgvCart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCart.GridColor = System.Drawing.Color.Black;
+            this.dgvCart.Location = new System.Drawing.Point(0, 0);
+            this.dgvCart.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.Size = new System.Drawing.Size(482, 240);
+            this.dgvCart.TabIndex = 0;
+            this.dgvCart.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCart_CellContentClick);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.BackColor = System.Drawing.Color.Blue;
+            this.btnFinish.Font = new System.Drawing.Font("Bookman Old Style", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinish.ForeColor = System.Drawing.Color.White;
+            this.btnFinish.Location = new System.Drawing.Point(64, 176);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(202, 50);
+            this.btnFinish.TabIndex = 6;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = false;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Change";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Amount Paid";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount Due";
+            // 
+            // txtChange
+            // 
+            this.txtChange.Location = new System.Drawing.Point(114, 115);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.Size = new System.Drawing.Size(96, 23);
+            this.txtChange.TabIndex = 2;
+            this.txtChange.TextChanged += new System.EventHandler(this.txtChange_TextChanged);
+            // 
+            // txtPaid
+            // 
+            this.txtPaid.Location = new System.Drawing.Point(114, 72);
+            this.txtPaid.Name = "txtPaid";
+            this.txtPaid.Size = new System.Drawing.Size(96, 23);
+            this.txtPaid.TabIndex = 1;
+            this.txtPaid.TextChanged += new System.EventHandler(this.txtPaid_TextChanged);
+            // 
+            // txtDue
+            // 
+            this.txtDue.Location = new System.Drawing.Point(114, 31);
+            this.txtDue.Name = "txtDue";
+            this.txtDue.Size = new System.Drawing.Size(96, 23);
+            this.txtDue.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Product";
+            this.Column1.Name = "Column1";
+            // 
+            // colPrice
+            // 
+            this.colPrice.HeaderText = "Price";
+            this.colPrice.Name = "colPrice";
+            this.colPrice.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Quantity";
+            this.Column2.Name = "Column2";
+            // 
+            // colExtPrice
+            // 
+            this.colExtPrice.HeaderText = "Ext Price";
+            this.colExtPrice.Name = "colExtPrice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(922, 385);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.ComboBox cboItems;
+        private System.Windows.Forms.DataGridView dgvCart;
+        private System.Windows.Forms.TextBox txtChange;
+        private System.Windows.Forms.TextBox txtPaid;
+        private System.Windows.Forms.TextBox txtDue;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colExtPrice;
+    }
+}
+
---
+++ POSEvening/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POSEvening
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        decimal total = 0;
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            int qty = 0;
+            if (cboItems.SelectedIndex == -1)
+            {
+                MessageBox.Show(" An item !");
+                cboItems.Focus();
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(txtQty.Text))
+            {
+                try
+                {
+                   qty = int.Parse(txtQty.Text);
+                   total = total + decimal.Parse((3.5 * qty) + "");
+                   txtDue.Text = total.ToString();  
+                }
+                catch(Exception eex){
+                    MessageBox.Show("Unacceptable Quantity " ,
+                        this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Console.WriteLine(eex.Message + " occurred in btnAddtoCart");
+                    return;
+                }
+               
+            }
+            else if (string.IsNullOrEmpty(txtQty.Text))
+            {
+                MessageBox.Show("Select the Quantity!");
+                txtQty.Focus();
+                return;
+            }
+
+            string item = cboItems.Text;
+            decimal price = 3.5m;
+            //int qty =0;
+            //int.TryParse(txtQty.Text, out qty);
+            decimal ExtPrice = price * qty;
+
+            string[] cartline = {
+                                     item, 
+                                     price.ToString("F2"),
+                                     qty.ToString(),
+                                     ExtPrice.ToString("F2")
+                                 };
+
+            dgvCart.Rows.Add(cartline);
+            ClearEntry();
+        }
+
+        private void ClearEntry()
+        {
+            cboItems.SelectedIndex = -1;
+            txtQty.Text = "";
+       
+        
+        }
+
+         private void texDue_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int qty = int.Parse(txtQty.Text);
+                string item = cboItems.Text;
+                decimal price = 3.5m;
+                decimal ExtPrice = price * qty;
+            }
+            catch (Exception xx)
+            {
+                MessageBox.Show("Invalid Entry");
+            }
+        }
+        private void btnFinish_Click_1(object sender, EventArgs e)
+        {
+            
+            decimal paid = decimal.Parse((txtPaid.Text) + "");
+            if (paid >= total)
+            {
+                
+                decimal change = paid - total;
+                txtChange.Text = change.ToString();
+                MessageBox.Show("Transaction successful!");
+            }
+            else
+            {
+               
+                MessageBox.Show("Amount quoted is less than product cost");
+            }
+        }
+
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dgvCart_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtChange_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPaid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
